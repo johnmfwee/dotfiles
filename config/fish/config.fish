4dcadbc6869b410d31fa09b5ead023531a94a3cb
John Wee <johnmfwee@gmail.com>
2022-01-09 07:48:47 +1100

    fix(config): updated config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -20,7 +20,7 @@ set -g theme_display_git_dirty_verbose yes
 set -g theme_display_git_stashed_verbose yes
 set -g theme_display_git_default_branch yes
 set -g theme_git_default_branches master main
-#set -g theme_git_worktree_support yes
+set -g theme_git_worktree_support yes
 set -g theme_use_abbreviated_branch_name yes
 set -g theme_display_vagrant yes
 set -g theme_display_docker_machine no
@@ -46,7 +46,7 @@ set -g theme_nerd_fonts yes
 set -g theme_show_exit_status yes
 set -g theme_display_jobs_verbose yes
 set -g default_user your_normal_user
-set -g theme_color_scheme dark
+set -g theme_color_scheme dracula
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_project_dir_length 1
 set -g theme_newline_cursor yes
